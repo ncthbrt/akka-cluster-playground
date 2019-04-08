b7f0d371d5505af7063d7d4d90a2577a6480e0bf
Jan <jan@whereismytransport.com>
2019-04-08 10:56:12 +0200

    fixed codebase :>

--- src/Playground.Protocol/ActorPaths.cs
+++ src/Playground.Protocol/ActorPaths.cs
@@ -3,11 +3,11 @@ using Playground.Shared;
 namespace Playground.Protocol
 {
     public static class ActorPaths
-    {    
+    {
         public static readonly ActorMetaData PubSubMediator = new ActorMetaData("distributedPubSubMediator", isSystemActor: true);
         public static readonly DistributedSingletonActorMetaData TicketCounterActor = new DistributedSingletonActorMetaData("ticket-counter", ActorRoles.Worker);
-        
-        public static readonly DistributedShardedActorMetaData<AnimalProtocol.AnimalMessageExtractor> AnimalActors = 
-            new DistributedShardedActorMetaData<AnimalProtocol.AnimalMessageExtractor>("animals", ActorRoles.Worker);                
+
+        public static readonly DistributedShardedActorMetaData<AnimalMessageExtractor> AnimalActors =
+            new DistributedShardedActorMetaData<AnimalMessageExtractor>("animals", ActorRoles.Worker);
     }
 }
